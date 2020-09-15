fcf7a6f9c404a6262eb332997ac8eda6b24b10d7
sieger <oluwaseun.v@yahoo.com>
2020-09-15 15:03:56 +0100

    sieger's page

---
+++ lets.js/lets.js
@@ -0,0 +1,11 @@
+function openNav()  {
+    document.querySelector("ul").style.width = "100%";
+    
+  }
+  
+  function closeNav() {
+    document.querySelector("ul").style.width = "0";
+    document.querySelector("main").style.marginLeft= "0";
+
+  }
+ 
